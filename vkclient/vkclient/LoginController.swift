11b9ef53adabcbee1aadb75e12162cbb201be5dc
Svetoslav <venom008@gmail.com>
2016-03-07 16:20:51 +0300

    test stuff

---
+++ vkclient/vkclient/LoginController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  vkclient
+//
+//  Created by Svetoslav Karasev on 07.03.16.
+//  Copyright © 2016 Svetoslav Karasev. All rights reserved.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+
+    @IBOutlet weak var loginField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func signIn(sender: UIButton) {
+        if loginField.hasText() && passwordField.hasText() {
+            let login = loginField.text!, password = passwordField.text!
+            
+            let alertController = UIAlertController(title: "Ololo", message: "\(login) \(password)",preferredStyle: .Alert)
+            
+            let button = UIAlertAction(title: "ok", style: .Cancel, handler: nil)
+            
+            alertController.addAction(button)
+       
+            presentViewController(alertController, animated: true, completion: nil)
+        }
+    }
+}
+
